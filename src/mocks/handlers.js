a0c1039ed0cc7a01d5518465dce2e71269c84b1f
Drozdov Evgeny <drozdove912@gmail.com>
2025-05-31 07:34:56 +0200

    data

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,14 +1,76 @@
 import { HttpResponse, http, delay } from "msw";
 let posts = [
-  { id: "1", title: "Повербанк 1", views: 100, isActive: false },
-  { id: "2", title: "Повербанк 2", views: 200, isActive: false },
-  { id: "3", title: "Повербанк 3", views: 100, isActive: false },
-  { id: "4", title: "Повербанк 4", views: 200, isActive: false },
-  { id: "5", title: "Повербанк 5", views: 100, isActive: false },
-  { id: "6", title: "Повербанк 6", views: 200, isActive: true },
-  { id: "7", title: "Повербанк 4", views: 200, isActive: false },
-  { id: "8", title: "Повербанк 5", views: 100, isActive: false },
-  { id: "9", title: "Повербанк 6", views: 200, isActive: false },
+  {
+    id: "1",
+
+    price: 0,
+    isActive: false,
+    endDate: "",
+    rentalStart: "",
+  },
+  {
+    id: "2",
+
+    price: 0,
+    isActive: false,
+    endDate: "",
+    rentalStart: "",
+  },
+  {
+    id: "3",
+
+    price: 0,
+    isActive: false,
+    endDate: "",
+    rentalStart: "",
+  },
+  {
+    id: "4",
+
+    price: 0,
+    isActive: false,
+    endDate: "",
+    rentalStart: "",
+  },
+  {
+    id: "5",
+
+    price: 0,
+    isActive: false,
+    endDate: "",
+    rentalStart: "",
+  },
+  {
+    id: "6",
+
+    price: 0,
+    isActive: true,
+    endDate: "",
+    rentalStart: "",
+  },
+  {
+    id: "7",
+    price: 0,
+    isActive: false,
+    endDate: "",
+    rentalStart: "",
+  },
+  {
+    id: "8",
+
+    price: 0,
+    isActive: false,
+    endDate: "",
+    rentalStart: "",
+  },
+  {
+    id: "9",
+
+    price: 0,
+    isActive: false,
+    endDate: "",
+    rentalStart: "",
+  },
 ];
 
 const postsResolver = async () => {
